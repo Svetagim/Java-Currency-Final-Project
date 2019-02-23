faa6e378b2c9942961beb3f7a7c40730ff6f3de1
svetagimpelson <svetagimpelson@se.shenkar.ac.il>
2019-02-23 22:51:21 +0200

    algoritm

--- src/GUI.java
+++ src/GUI.java
@@ -1,13 +1,14 @@
-import org.w3c.dom.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import javax.swing.*;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
 
-class GUI
-{
-    public static void main(String args[])
-    {
+class GUI {
+    public static void main(String args[]) {
         try {
             File fXmlFile = new File("currency.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
@@ -15,45 +16,69 @@ class GUI
             Document docLocal = dBuilder.parse(fXmlFile);
             NodeList nList = docLocal.getElementsByTagName("CURRENCY");
             String data[][] = new String[nList.getLength()][6];
-                for (int i = 0; i < nList.getLength(); i++) {
-                    Node nNode = nList.item(i);
-                    if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                        Element eElement = (Element) nNode;
-                        data[i][0] = eElement.getElementsByTagName("NAME").item(0).getTextContent();
-                        data[i][1] = eElement.getElementsByTagName("UNIT").item(0).getTextContent();
-                        data[i][2] = eElement.getElementsByTagName("CURRENCYCODE").item(0).getTextContent();
-                        data[i][3] = eElement.getElementsByTagName("COUNTRY").item(0).getTextContent();
-                        data[i][4] = eElement.getElementsByTagName("RATE").item(0).getTextContent();
-                        data[i][5] = eElement.getElementsByTagName("CHANGE").item(0).getTextContent();
-                    }
+            for (int i = 0; i < nList.getLength(); i++) {
+                Node nNode = nList.item(i);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    data[i][0] = eElement.getElementsByTagName("NAME").item(0).getTextContent();
+                    data[i][1] = eElement.getElementsByTagName("UNIT").item(0).getTextContent();
+                    data[i][2] = eElement.getElementsByTagName("CURRENCYCODE").item(0).getTextContent();
+                    data[i][3] = eElement.getElementsByTagName("COUNTRY").item(0).getTextContent();
+                    data[i][4] = eElement.getElementsByTagName("RATE").item(0).getTextContent();
+                    data[i][5] = eElement.getElementsByTagName("CHANGE").item(0).getTextContent();
                 }
-            String column[] = {"NAME","UNIT","CURRENCYCODE","COUNTRY","RATE","CHANGE"};
+            }
+            String column[] = {"NAME", "UNIT", "CURRENCYCODE", "COUNTRY", "RATE", "CHANGE"};
+
+            //Rate exchange
+
+            String from = "LBP", to = "ILS";
+            double ammount = 1;
+            double fromRate = 1.0, fromUnit = 1.0, toRate = 1.0, toUnit = 1.0;
+            double exchangeRate;
+            for (int i = 0; i < nList.getLength(); i++){
+                if (data[i][2].equals(from)) {
+                    fromRate = Double.parseDouble(data[i][4]);
+                    fromUnit = Double.parseDouble(data[i][1]);
+                }
+                else if (data[i][2].equals(to)) {
+                    toRate = Double.parseDouble(data[i][4]);
+                    toUnit = Double.parseDouble(data[i][1]);
+                }
+            }
+
+            exchangeRate = (fromRate/toRate)*(toUnit/fromUnit)*ammount;
+            System.out.println(exchangeRate);
+
+
+            //GUI
+
             JFrame f = new JFrame("Java Currency");
             JLabel lblAmount, lblFrom, lblTo;
             JTextField txtAmount, txtFrom, txtTo;
             JButton btnGo;
 
             lblAmount = new JLabel("Amount");
-            lblAmount.setBounds(50,50, 100,30);
+            lblAmount.setBounds(50, 50, 100, 30);
             txtAmount = new JTextField("");
-            txtAmount.setBounds(150,50, 200,30);
+            txtAmount.setBounds(150, 50, 200, 30);
 
             lblFrom = new JLabel("From");
-            lblFrom.setBounds(50,80, 100,30);
+            lblFrom.setBounds(50, 80, 100, 30);
             txtFrom = new JTextField("");
-            txtFrom.setBounds(150,80, 200,30);
+            txtFrom.setBounds(150, 80, 200, 30);
 
             lblTo = new JLabel("To");
-            lblTo.setBounds(50,110, 100,30);
+            lblTo.setBounds(50, 110, 100, 30);
             txtTo = new JTextField("");
-            txtTo.setBounds(150,110, 200,30);
+            txtTo.setBounds(150, 110, 200, 30);
 
             btnGo = new JButton("Find");
-            btnGo .setBounds(40,140,80,30);
+            btnGo.setBounds(40, 140, 80, 30);
 //https://www.javatpoint.com/java-jtable
-            JTable jt = new JTable(data,column);
-            jt.setBounds(20,200,1000,400);
-            //JScrollPane sp = new JScrollPane(jt);
+            JTable jt = new JTable(data, column);
+            jt.setBounds(20, 200, 1000, 400);
+            JScrollPane sp = new JScrollPane(jt);
 
             f.add(lblAmount);
             f.add(txtAmount);
@@ -64,12 +89,14 @@ class GUI
             f.add(btnGo);
             f.add(jt);
 
-            f.setSize(1200,900);
+            f.setSize(1200, 900);
             f.setLayout(null);
             f.setVisible(true);
-            }
-            catch (Exception e) {
-                e.printStackTrace();
-            }
+        } catch (
+                Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
--- src/GetXML.java
+++ src/GetXML.java
@@ -8,13 +8,12 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 import java.io.*;
 
 
-public class Samplee
+public class GetXML
 {
     public static void main(String args[])
     {
